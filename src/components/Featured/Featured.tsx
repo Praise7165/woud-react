65d77d705c6893d4df51683ad6b210efa1c085a8
Omolade Lekan <omoladelekan99@gmail.com>
2026-04-20 09:09:07 +0100

    completed category section for now

--- src/components/Featured/Featured.tsx
+++ src/components/Featured/Featured.tsx
@@ -6,18 +6,17 @@ export default function Featured() {
   return (
     <section className="py-26 bg-[#fbfbfb]">
       <Container>
-        <div className="flex mb-16 flex-col gap-4 justify-between md:gap-12 md:flex-row md:items-end lg:gap-26">
+        <div className="mb-10 md:mb-16">
           <div>
             <span className="text-[#dc2f02] leading-normal font-medium">
               Products
             </span>
             <h2>Our Featured Products</h2>
           </div>
-          <div className="flex gap-6"></div>
         </div>
 
         {/*className = 'product-wrapper' */}
-        <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-x-4 gap-y-12 md:grid-cols-[repeat(auto-fit,minmax(280px,1fr))] md:gap-x-6">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(240px,1fr))] gap-x-4 gap-y-9 lg:gap-y-12 md:grid-cols-[repeat(auto-fit,minmax(280px,1fr))] md:gap-x-6">
           {products.map((product) => (
             <ProductCard
               key={product.id}
